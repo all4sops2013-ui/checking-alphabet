23523590888494981ea8a82cc115d769f100fd16
all4sops2013-ui <all4sops2013@gmail.com>
2025-09-14 12:57:52 +0100

    Add files via upload

---
+++ ASCII value.py
@@ -0,0 +1,7 @@
+character = 'A'
+ascii_value = ord(character)
+print(f"The ASCII value of '{character}' is {ascii_value}")
+
+character = 'a'
+ascii_value = ord(character)
+print(f"The ASCII value of '{character}' is {ascii_value}")
